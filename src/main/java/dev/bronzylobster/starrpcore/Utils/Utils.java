f766fc312b00eab472d474cdb402a472d8d06eec
BL40 <kek.gordon@gmail.com>
2025-01-14 03:04:01 +0300

    1.17.1-63

--- src/main/java/dev/bronzylobster/starrpcore/Utils/Utils.java
+++ src/main/java/dev/bronzylobster/starrpcore/Utils/Utils.java
@@ -12,6 +12,9 @@ import org.apache.commons.lang3.math.NumberUtils;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Utils {
 
     public static int getNumber (String sNum, CommandSender p) throws NumberFormatException{
@@ -41,5 +44,41 @@ public class Utils {
             p.sendMessage(Component.text("Value is not number").color(TextColor.color(0xFF0000)).clickEvent(ClickEvent.openUrl("https://www.youtube.com/watch?v=L1YkydtyTt8")));
             return  null;
         }
+    }
+
+    public static long parseTime(String timeString) {
+        long totalTicks = 0;
+        Pattern pattern = Pattern.compile("(\\d+)([smhdwM])");
+        Matcher matcher = pattern.matcher(timeString);
+
+        while (matcher.find()) {
+            int value = Integer.parseInt(matcher.group(1));
+            String unit = matcher.group(2);
+
+            switch (unit) {
+                case "s":
+                    totalTicks += value * 20L;
+                    break;
+                case "m":
+                    totalTicks += value * 1200L;
+                    break;
+                case "h":
+                    totalTicks += value * 72000L;
+                    break;
+                case "d":
+                    totalTicks += value * 1728000L;
+                    break;
+                case "w":
+                    totalTicks += value * 12096000L;
+                    break;
+                case "M":
+                    totalTicks += value * 518400000L;
+                    break;
+                default:
+                    return -1;
+            }
+        }
+
+        return totalTicks;
     }
 }
